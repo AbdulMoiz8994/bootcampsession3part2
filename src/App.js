95fed6491d3afee623bd22eea30ae6a93bf71066
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-06 02:42:42 +0500

    we add the css external and made the function via using variablle useStae
    we perform the action throught tornery operator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React ,{useState}from 'react'
 import './App.css';
+//import Component
+import {Child} from './Child'
 
 function App() {
 let [count,setCount]=useState(0) //0 Will be initial value of count
-//setCount to do the update to count value
+let[isMorning, updateDay]=useState()
+// console.log(count)
+//setCount to do the up date to count value
   return (
-    <div className="App">
-      <h1>We are doing Counting Variable= {count}</h1>
+    <div className={ isMorning ? 'morning' : 'night'}>
+      <h2>Day Time= {isMorning ? "Good Morning" : "Good Night"}</h2>
+      <Child counter={count}/>
+      {/* <h1>We are doing Counting Variable= {count}</h1> */}
       <button onClick={() => setCount(++count)}>Update Value</button>
+      <button onClick={() => updateDay(!isMorning)}>Update Day</button>
     </div>
   );
 }
---
+++ src/Child.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Child = (props) => {
+    // console.log(props)
+    return (
+        <div>
+        <h1>The Counter's Variable value= {props.counter}</h1>         
+        </div>
+    )
+}
